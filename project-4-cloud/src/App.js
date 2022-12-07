21f6ea49035c353b93cd1f7f1efa6786a0248fa6
Alperen <bakirci.alperen@gmail.com>
2022-12-07 12:07:18 -0600

    initial check in

--- project-4-cloud/src/App.js
+++ project-4-cloud/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Alperen BAKIRCI
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Little change
+          Learn React
         </a>
       </header>
     </div>
